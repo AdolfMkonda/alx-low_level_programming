4caa91d38fb4906935aefe49f0a3047f267932cf
AdolfMkonda <adolfanatory@gmail.com>
2023-02-17 07:19:27 -0800

    sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - A program that prints the size of various type
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int g;
+	long long int h;
+	float j;
+
+	printf("Size of char: &lu byte(s)\n", sizeof(a));
+	printf("Size of char: &lu byte(s)\n", sizeof(b));
+	printf("Size of char: &lu byte(s)\n", sizeof(g));
+	printf("Size of char: &lu byte(s)\n", sizeof(h));
+	printf("Size of char: &lu byte(s)\n", sizeof(j));
+	return (0);
+}
