dc710869c454d669045b03036be891ba099df98f
AdolfMkonda <adolfanatory@gmail.com>
2023-02-17 06:47:35 -0800

    print function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,13 +2,9 @@
 /**
  * main - printing text using printf function
  * Return: 0 (success)
- *
- *
- *
- *
  */
 int main(void)
 {
-	printf("with proper grammar\, but the outcome is a piece of art\,");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
