707ea23bdde4de7e57c7e4289818ce746c7201df
AdolfMkonda <adolfanatory@gmail.com>
2023-02-17 19:49:58 -0800

    89

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - A program to print reverse alphabet
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char c;
+	
+	c = 'z';
+	while
+		(c >= 'a')
+		{
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}
