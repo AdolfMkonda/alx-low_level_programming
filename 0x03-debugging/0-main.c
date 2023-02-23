6a1532597840d81d2975f51c5e3032b93232e917
AdolfMkonda <adolfanatory@gmail.com>
2023-02-23 11:01:15 -0800

    00

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - A function that checks for negative/positive integer
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+	
+	i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
