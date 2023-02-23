0b0882fcaa91bacfab88af571af38c5b7d255bd4
AdolfMkonda <adolfanatory@gmail.com>
2023-02-22 16:49:07 -0800

    55

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - print sign of a number
+ * @n: the int to chek
+ * Return: 1 for V+ else V-
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		putchar(48);
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+	}
+		return (-1);
+}
