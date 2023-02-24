2439a3932605dbb29d176ddf0696172b71b7a016
AdolfMkonda <adolfanatory@gmail.com>
2023-02-23 17:14:12 -0800

    00

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - it checks upper letter
+ * @x: num to be checked
+ * Return: 1 for upper else 0
+ */
+int _isupper(int x)
+{
+	if (x >= 65 && x <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
