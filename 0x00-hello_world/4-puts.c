2b30760a2315db42b967430d519c82ae3d5b8cf9
AdolfMkonda <adolfanatory@gmail.com>
2023-06-23 01:47:18 +0300

    introduction to C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - program that use puts function to prints word
+ * Return: always 0
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
