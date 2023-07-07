083cdf889a09c2d40a1da4804e8b6344dd39a565
AdolfMkonda <adolfanatory@gmail.com>
2023-07-07 13:46:44 +0300

    append

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcat - a program to copy string
+ * @src: source string
+ * @dest: destination string
+ * Return: 0
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int count;
+	int s;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		count++;
+	}
+	for (s = 0; s < count; s++)
+	{
+		dest[s] = src[s];
+	}
+	return (dest);
+}
