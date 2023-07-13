02845f8c01255efde1a0528da3b2f3cb422ac4e3
AdolfMkonda <adolfanatory@gmail.com>
2023-07-13 12:48:46 +0300

    more malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - program that return pointer to alloc
+ * @b: number of bytes to be returned
+ * Return: 0
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		return (98);
+	return (p);
+}
