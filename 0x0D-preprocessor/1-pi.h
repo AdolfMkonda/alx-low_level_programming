cda87bd4f0556283344491e3a5baf642a93f5b44
AdolfMkonda <adolfanatory@gmail.com>
2023-07-18 04:18:16 +0300

    fix errors

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,5 +1,6 @@
 #ifndef PI_H
 #define PI_H
-//macro for PI
+
 #define PI 3.14159265359
+
 #endif
--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,7 @@
 #ifndef ABS_H
 #define ABS_H
+
 //macro that calc absolute value
-#define ABS(x) ((x) < 0 ? -(x) : (x))
+#define ABS(x) ((x) < (0) ? -(x) : (x))
+
 #endif
