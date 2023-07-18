9798df923e2bda196c435b17f4805195aa4bd8b2
AdolfMkonda <adolfanatory@gmail.com>
2023-07-18 04:08:50 +0300

    macros

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,5 @@
+#ifndef _HEADER_
+#define _HEADER_
+//macro for PI
+#define PI 3.14159265359
+#endif
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,5 @@
+#ifndef _HEADER_
+#define _HEADER_
+//macro that calc absolute value
+#define ABS(x) ((x) < 0 ? ((x) * -1) : (x))
+#endif
