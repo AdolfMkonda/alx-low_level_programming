5eb0c5de75e90ad1df317aa31187ef845b30d019
AdolfMkonda <adolfanatory@gmail.com>
2023-07-21 05:46:00 +0300

    fixing error on file sum_them_all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -16,7 +16,7 @@ int sum_them_all(const unsigned int n, ...)
 	va_start (args, n);
 	for (i = 0; i < n; i++)
 	{
-		num = va_arg(args int);
+		num = va_arg(args, int);
 		total += num;
 	}
 	va_end(args);
