8c101497755abc78c70792d2066e8397044a5794
AdolfMkonda <adolfanatory@gmail.com>
2023-10-28 17:38:28 +0300

    fixing error

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,29 +6,22 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result;
-	int index = 0;
+	unsigned int num = 0;
 
-	result = 0;
-
-	if (!b)
+	if (b == NULL)
 		return (0);
 
-	while (b[index] != '\0')
+	while (*b != '\0')
 	{
-		char currentChar = b[index];
-
-		if (currentChar == '0')
-		{
-			result = (result << 1);
-		}
-		else if (currentChar == '1')
-		{
-			result = (result << 1) | 1;
-		}
-		else
+		if (*b != '0' && *b != '1')
 			return (0);
-		index++;
+
+		num <<= 1;
+
+		if (*b == '1')
+			num += 1;
+
+		b++;
 	}
-	return (result);
+	return (num);
 }
