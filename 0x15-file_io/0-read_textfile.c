360de21ffa3472dd94ab257cbda7156745a1d7b0
AdolfMkonda <adolfanatory@gmail.com>
2024-02-12 23:05:10 +0300

    fixing error on file reading

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,17 +12,17 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buffer;
 	size_t w;
-	FILE *fp;
+	int fp;
 
-	if (filename != 1)
+	if (filename == 0)
 		return (0);
 	buffer = malloc(sizeof(char) * (letters));
-	fp = open(filename, "r");
+	fp = open(filename, O_RDONLY);
 	if (fp == -1)
 		return (0);
 	w = write(STDOUT_FILENO, buffer, letters);
 
-	if (w == -1)
+	if (!w)
 		return (0);
 	close(fp);
 	free(buffer);
